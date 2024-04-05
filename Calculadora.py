97584ac4dad3b87dcccf2ecb12baf6acf10b9c0c
AlexGTroncoso <alexandertroncoso2000@gmail.com>
2024-04-05 09:19:21 -0500

    Agregué la función Error relativo porcentual

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,18 @@
+def producto(a,b)
+    #función que suma los valores a y b
+    return a*b
+
+def remainder(x,a):
+    return x%a
+
+def resta(a,b):
+    # función que resta los valores a y b
+    return a-b
+
+def suma(a,b):
+	# Función que suma los valores a y b
+	return a+b
+	
 def error_relativo_porcentual(valor_aproximado, valor_exacto):
     """
     Calcula el error relativo porcentual dado un valor aproximado y un valor exacto.
